d43083a61f8666bcf18fc8b2c4fd3a91fe4bcb0e
VladMihai23 <vlamivox@yahoo.com>
2025-09-03 17:35:00 +0300

    commit

--- obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb5027e3a539f433463f54fd76eaa753421988b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1988c7ec8d51daf0b45bf9b9345a7ff5630c7054")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
